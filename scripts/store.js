46f87fca92e7bd1ede4caff2213d5823f2a4e031
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-10-30 14:39:53 -0600

    partial exercise 6

--- scripts/store.js
+++ scripts/store.js
@@ -4,46 +4,46 @@ let items = [];
 let hideCheckedItems = false;
 
 function findById(id) {
-    items.find(ele => {
-        return items.ele;
-    });
+  items.find(ele => {
+    return items.ele;
+  });
 }
 
 function addItem(name) {
-    try {
-        item.validateName(name);
-        this.items.push(item.create(name));
-    } catch (error) {
-        console.log(`Cannot add item: ${error.message}`);
-    }
+  try {
+    item.validateName(name);
+    this.items.push(item.create(name));
+  } catch (error) {
+    console.log(`Cannot add item: ${error.message}`);
+  }
 
 }
 
 function findAndToggleChecked(id) {
-    this.findById();
+  this.findById();
 
 }
 
 function findAndUpdateName(id) {
-    try {
-        item.validateName(name);
-        item.findById();
-    } catch (error) {
-        console.log(`Cannot update name: ${error.message}`);
-    }
+  try {
+    item.validateName(name);
+    item.findById();
+  } catch (error) {
+    console.log(`Cannot update name: ${error.message}`);
+  }
 
 }
 
 function findAndDelete(id) {
-    items.splice(findById(id), 1);
+  items.splice(findById(id), 1);
 }
 
 export default {
-    items,
-    hideCheckedItems,
-    addItem,
-    findAndDelete,
-    findById,
-    findAndToggleChecked,
-    findAndUpdateName
+  items,
+  hideCheckedItems,
+  addItem,
+  findAndDelete,
+  findById,
+  findAndToggleChecked,
+  findAndUpdateName
 };
