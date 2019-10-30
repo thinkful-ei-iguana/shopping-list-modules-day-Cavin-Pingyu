18b0e61fd5ac32dd4b044ec30d98ab27539b7f43
john94 <ethanok0802@gmail.com>
2019-10-30 11:01:06 -0700

    minor changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 import shoppingList from './shopping-list.js';
 
-const main = function () {
-  shoppingList.bindEventListeners();
-  shoppingList.render();
+const main = function() {
+    shoppingList.bindEventListeners();
+    shoppingList.render();
 };
 
 $(main);
