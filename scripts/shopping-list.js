878bfcd74aa57b6b4c5b3343c1686a236ca791fc
john94 <ethanok0802@gmail.com>
2019-10-30 12:57:03 -0700

    exercise 5

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -45,7 +45,7 @@ const render = function() {
 const addItemToShoppingList = function(itemName) {
     try {
         item.validateName(itemName);
-        store.items.push(item.create(itemNa me));
+        store.items.push(item.create(itemName));
 
     } catch (error) {
         console.log(`Cannot add item: ${error.message}`);
--- scripts/store.js
+++ scripts/store.js
@@ -1,10 +1,45 @@
 import item from './item.js';
+
 let items = [];
 let hideCheckedItems = false;
 
+function findById(id) {
+    store.items.find(ele => {
+        return store.items.ele;
+    })
+};
+
+function addItem(name) {
+    try {
+        item.validateName(name);
+        this.items.push(item.create(name));
+    } catch (error) {
+
+    }
+    render();
+};
+
+function findAndToggleChecked(id) {
+    this.findById();
+    render();
+}
+
+function findAndUpdateName(id) {
+    try {
+        item.validateName(name);
+        item.findById();
+    } catch (error) {
+        console.log(`Cannot update name: ${error.message}`);
+    }
+    render();
+}
+
+function findAndDelete(id) {
+    this.items.splice(item.find(id), 1);
+    render();
+}
 
 export default {
     items,
     hideCheckedItems,
 };
-
